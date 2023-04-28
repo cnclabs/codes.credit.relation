e84eaee8bda56f7683109578a8666a1f8917a4ba
Benson Chen <105162010+BensonChen920397@users.noreply.github.com>
2023-04-28 16:54:51 +0800

    Upload experiments folder

---
+++ experiments/exe_mk_folds.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./mk_folds.sh fim time
+./mk_folds.sh fim_index index
+
+for i in 01 06 12;
+do
+    ./mk_folds.sh mlp${i} time
+    ./mk_folds.sh gru${i} time
+    ./mk_folds.sh lstm${i} time
+    ./mk_folds.sh mlp${i}_index index
+    ./mk_folds.sh gru${i}_index index
+    ./mk_folds.sh lstm${i}_index index
+done
---
+++ experiments/mk_models.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir fim
+mkdir fim_index
+
+for i in 01 06 12;
+do
+    mkdir mlp${i}
+    mkdir gru${i}
+    mkdir lstm${i}
+    mkdir mlp${i}_index
+    mkdir gru${i}_index
+    mkdir lstm${i}_index
+done
